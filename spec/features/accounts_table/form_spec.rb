d19cd0265679362c1b3e6d6b160aa26cd3f98fa9
Daniel Park <dpark189@gmail.com>
2018-10-28 19:21:44 -0400

    added test to check for when account is marked as completed

--- spec/features/accounts_table/form_spec.rb
+++ spec/features/accounts_table/form_spec.rb
@@ -21,4 +21,24 @@ feature 'Account Form' do
     expect(page).to have_content("10/24/2018")
     expect(page).to_not have_content("10/25/2018")
   end
+
+  scenario "Marking account as complete" do
+    date = "10/24/2018"
+    login_user(a_user)
+    create_test_account1
+    account = Account.first
+    visit_accounts_index_for_date(date, "nj")
+    find(:css, ".completed").set(true)
+
+    find(:css, ".amount_received").set("#{account.amount_owed}")
+
+    find(:css, ".received_as_cash_or_check").set("#{account.amount_owed}")
+
+    find(:css, ".logistics_agent_initials").set("AD")
+
+    find(:css, ".reason_details").set("asdf")
+
+    click_button("Submit")
+    expect(page).to_not have_content("#{account.customer_id}")
+  end
 end
