48460f537ea1b011b028cf1f9eef46eef5ad646b
Daniel Park <dpark189@gmail.com>
2018-11-05 15:20:04 -0500

    added tinytd methods

--- tinytd_test.rb
+++ tinytd_test.rb
@@ -100,3 +100,41 @@ def sample_query
    "order_terms"=>"CODC"}]
    return arr
 end
+
+def query_test
+  column_query = "SELECT
+    a.cost_ctr,
+    a.cust_code,
+    a.cust_name,
+    b.invoice_numb,
+    b.ship_date,
+    a.route_code,
+    round(b.sub_total + b.charged_frt,2) AS Total,
+    b.sub_total + b.charged_frt AS Tot_Org,
+    a.order_terms
+  FROM
+    ordhead a, invheadview b
+  WHERE a.order_numb = b.order_numb AND a.rel_numb = b.rel_numb AND order_terms LIKE 'COD%'"
+
+  client = TinyTds::Client.new username: "intranet", password: "Duck600it", adapter: "sqlserver", host: "sqlserver01", database: "e21prod"
+  result = client.execute("#{column_query}")
+  arr = []
+  result.each do |row|
+    arr << row
+  end
+  return arr
+end
+
+def prod_client
+  client = TinyTds::Client.new username: "intranet", password: "Duck600it", adapter: "sqlserver", host: "sqlserver01", database: "e21prod"
+  return client
+end
+
+def table_names(client)
+  result = client.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES")
+  arr = []
+  result.each do |row|
+    arr << row["TABLE_NAME"]
+  end
+  return arr
+end
